2ba1e9e32bcafefc948a07e964705920d36fdfe6
ashkanhpb <ashkanhpb@gmail.com>
2024-06-21 16:21:46 +0330

    app
    add app to setting.py
    add urlmain
    add urls of app
    integrate base and templates
    make templates
    set views.py

---
+++ website/views.py
@@ -0,0 +1,11 @@
+import http.client
+
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index_view(request):
+    return render(request,'home.html')
+def contact_view(request):
+    return render(request,'contact.html')
+def about_view(request):
+    return render(request,'about.html')
